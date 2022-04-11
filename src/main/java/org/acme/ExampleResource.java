3d91811cfba854e0fd38ae3ee155366bf72cd267
knifehand <mhurt@msudenver.edu>
2022-04-10 19:43:17 -0600

    update

--- src/main/java/org/acme/ExampleResource.java
+++ src/main/java/org/acme/ExampleResource.java
@@ -4,8 +4,9 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.io.IOException;
 
-@Path("/api/salon")
+@Path("/color-crush")
 public class ExampleResource {
 
     @GET
@@ -13,4 +14,15 @@ public class ExampleResource {
     public String hello() {
         return "Hello RESTEasy";
     }
+
+//    @GET
+//    @Produces(MediaType.TEXT_HTML)
+//    @Path("/login")
+//    public int loginPage() throws IOException{
+//        String path = "~/IdeaProjects/color-crush/resources/";
+//        String fullPath = path + "index.html";
+//        return Files.readString(Paths.get(fullPath));
+//        return 0;
+//    }
+
 }
